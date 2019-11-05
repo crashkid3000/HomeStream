4683075e8cc6634d06b2ebcce21502fc5d61ccdd
BatiPlays <42764859+BatiPlays@users.noreply.github.com>
2019-11-05 14:19:51 +0100

    Aktuelle Version vom Server

---
+++ HomeStream/HomeStream/src/main/java/com/homestream/HomeStream/main/exception/LineFormatMismatch.java
@@ -0,0 +1,11 @@
+package com.homestream.HomeStream.main.exception;
+
+import java.io.IOException;
+
+public class LineFormatMismatch extends IOException
+{
+    public LineFormatMismatch()
+    {
+        super();
+    }
+}
