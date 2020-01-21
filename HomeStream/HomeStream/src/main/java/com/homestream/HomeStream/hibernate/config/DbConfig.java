f80b07da38d13480a7eb7aaaadc9fb2428f756f5
crashkid3000 <justinT.braack@gmx.de>
2020-01-21 12:47:46 +0100

    DAO 0.1.3
    The quest for a working ORM continues

--- HomeStream/HomeStream/src/main/java/com/homestream/HomeStream/hibernate/config/DbConfig.java
+++ HomeStream/HomeStream/src/main/java/com/homestream/HomeStream/hibernate/config/DbConfig.java
@@ -6,8 +6,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 /**
  * Some further configuration for Hibernate
@@ -40,6 +44,35 @@ public class DbConfig {
         dataSource.setUsername(env.getProperty("user"));
         dataSource.setPassword((env.getProperty("password")));
         return dataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan(
+                "com.homestream.HomeStream.dao", "com.homestream.HomeStream.entity", "com.homestream.HomeStream.vo");
+        sessionFactory.setHibernateProperties(hibernateProperties());
+
+        return sessionFactory;
+    }
+
+    @Bean
+    public PlatformTransactionManager hibernateTransactionManager() {
+        HibernateTransactionManager transactionManager
+                = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory().getObject());
+        return transactionManager;
+    }
+
+    private final Properties hibernateProperties() {
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty(
+                "hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2dll.auto"));
+        hibernateProperties.setProperty(
+                "hibernate.dialect", env.getProperty("hibernate.dialect"));
+
+        return hibernateProperties;
     }
 
 }
